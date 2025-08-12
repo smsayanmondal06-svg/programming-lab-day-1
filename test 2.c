7c1377acb90f86f8761c6d233e84537ecfe62414
smsayanmondal06-svg <smsayanmondal06@gmail.com>
2025-08-12 17:15:05 +0530

    Add files via upload

---
+++ test 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a");
+    scanf("%f",&a);
+    printf("enter b");
+    scanf("%f",&b);
+    printf("sum is : %f",a+b);
+    return 0;
+}
