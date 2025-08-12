bf9757dce509ee8fb0018325e1ce9549dfa7e5e3
smsayanmondal06-svg <smsayanmondal06@gmail.com>
2025-08-12 17:15:36 +0530

    Add files via upload

---
+++ test 4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    printf(" %d",a*1);
+    printf(" %d",a*2);
+    printf(" %d",a*3);
+    printf(" %d",a*4);
+    printf(" %d",a*5);
+    printf(" %d",a*6);
+    printf(" %d",a*7);
+    printf(" %d",a*8);
+    printf(" %d",a*9);
+    printf(" %d",a*10);
+    return 0;
+}
